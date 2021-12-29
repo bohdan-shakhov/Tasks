4beb74ec1390d3f48535c8503e98fc502f1b3940
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 14:49:11 +0200

    add solving the task about min different characters in word

---
+++ src/main/java/tasks/t4/Word.java
@@ -0,0 +1,27 @@
+package tasks.t4;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+public class Word {
+    public static void main(String[] args) {
+        String text = "fffff ab hfhfhf abc fa 1234 jkjk";
+        specialWord(text);
+    }
+
+    static void specialWord(String text) {
+        String[] words = text.split(" ");
+        String result = words[0];
+        int resultSize = new HashSet<>(Arrays.asList(words[0].split(""))).size();
+
+        for (String word : words) {
+            Set<String> set = new HashSet<>(Arrays.asList(word.split("")));
+            if (set.size() < resultSize) {
+                result = word;
+                resultSize = set.size();
+            }
+        }
+        System.out.println(result);
+    }
+}
