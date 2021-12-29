0288196db4aa2f7595a00fe1f03adc08790c58fe
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 18:09:17 +0200

    add test for ReverseString

---
+++ src/test/java/tasks/t5/ReverseStringTest.java
@@ -0,0 +1,14 @@
+package tasks.t5;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ReverseStringTest {
+    @Test
+    void test() {
+        String text = "test";
+        String s = ReverseString.reverseString(text);
+        assertEquals("tset", s);
+    }
+}
