d56695b6efe8552e724bae296ecb9e1bf9efb42b
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 17:37:55 +0200

    updating for test

--- src/main/java/tasks/t1/Fibbonachi.java
+++ src/main/java/tasks/t1/Fibbonachi.java
@@ -5,16 +5,21 @@ public class Fibbonachi {
         fibbonachiSequence(10);
     }
 
-    static void fibbonachiSequence(int n) {
+    static int[] fibbonachiSequence(int n) {
+        int[] fnums = new int[n];
         int num1 = 1;
         int num2 = 1;
+        fnums[0] = num1;
+        fnums[1] = num2;
         int num3;
         System.out.print(num1 + " " + num2 + " ");
         for (int i = 3; i < n + 1; i++) {
             num3 = num1 + num2;
+            fnums[i - 1] = num3;
             System.out.print(num3 + " ");
             num1 = num2;
             num2 = num3;
         }
+        return fnums;
     }
 }
