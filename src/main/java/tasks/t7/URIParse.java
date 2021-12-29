bb13d0547d8cbd90a3b23accc85b01384c246b61
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 16:40:01 +0200

    add solving the task about uri parse

---
+++ src/main/java/tasks/t7/URIParse.java
@@ -0,0 +1,55 @@
+package tasks.t7;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class URIParse {
+
+    private String protocol;
+    private String host;
+    private String port;
+    private String path;
+    private String query;
+
+    public URIParse(String protocol, String host, String port, String path, String query) {
+        this.protocol = protocol;
+        this.host = host;
+        this.port = port;
+        this.path = path;
+        this.query = query;
+    }
+
+    public static void main(String[] args) {
+        String text = "http://www.ietf.org:80/rfc/rfc2396.txt?q=1&s1";
+        URIParse result = parse(text);
+        System.out.println(result);
+    }
+
+    static URIParse parse(String uri) {
+        String protocol = "";
+        String host = "";
+        String port = "";
+        String path = "";
+        String query = "";
+        String regexp = "^([^:/?#]+)://([^/?#]*):(\\d+)([^?#]*)\\?(.*)";
+        Pattern pattern = Pattern.compile(regexp);
+        Matcher matcher = pattern.matcher(uri);
+        while (matcher.find()) {
+            protocol = matcher.group(1);
+            host = matcher.group(2);
+            port = matcher.group(3);
+            path = matcher.group(4);
+            query = matcher.group(5);
+        }
+        return new URIParse(protocol, host, port, path, query);
+    }
+
+    @Override
+    public String toString() {
+        return "protocol -> " + protocol + System.lineSeparator() +
+                "host -> " + host + System.lineSeparator() +
+                "port -> " + port + System.lineSeparator() +
+                "path -> " + path + System.lineSeparator() +
+                "query -> " + query + System.lineSeparator();
+    }
+}
