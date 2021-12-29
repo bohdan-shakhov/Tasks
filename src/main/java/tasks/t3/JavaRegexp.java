322edfdaf9f34af73dbf38640d87faf6cbc4cc3b
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 17:53:30 +0200

    update for testing

--- src/main/java/tasks/t3/JavaRegexp.java
+++ src/main/java/tasks/t3/JavaRegexp.java
@@ -1,19 +1,23 @@
 package tasks.t3;
 
+import java.util.ArrayList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class JavaRegexp {
     public static void main(String[] args) {
         String text = "Versions: Java  5, Java 6, Java   7, Java 8, Java 12.";
-        javaSubstring(text);
+        ArrayList<String> javax = javaSubstring(text);
+        System.out.println(javax);
     }
-    static void javaSubstring(String text) {
+    static ArrayList<String> javaSubstring(String text) {
+        ArrayList<String> list = new ArrayList<>();
         String regexp = "Java\\s+\\d{1,2}";
         Pattern pattern = Pattern.compile(regexp);
         Matcher matcher = pattern.matcher(text);
         while (matcher.find()) {
-            System.out.println(matcher.group());
+            list.add(matcher.group());
         }
+        return list;
     }
 }
