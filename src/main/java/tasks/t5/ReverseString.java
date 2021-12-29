6f7d5391be77d55e930a6da12eae9156b93ddb7d
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 18:04:05 +0200

    updating for testing

--- src/main/java/tasks/t5/ReverseString.java
+++ src/main/java/tasks/t5/ReverseString.java
@@ -3,14 +3,14 @@ package tasks.t5;
 public class ReverseString {
     public static void main(String[] args) {
         String text = "test";
-        reverseString(text);
+        System.out.println(reverseString(text));
     }
 
-    static void reverseString(String text) {
+    static String reverseString(String text) {
         String reversedString = "";
         for (int i = text.length() - 1; i >= 0; i--) {
             reversedString += text.charAt(i);
         }
-        System.out.println(reversedString);
+        return reversedString;
     }
 }
