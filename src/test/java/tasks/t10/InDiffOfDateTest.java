3d710b895af367c4b66c68cea0ba43cd2f705c53
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 18:20:44 +0200

    add test for InDiffOfDate task

---
+++ src/test/java/tasks/t10/InDiffOfDateTest.java
@@ -0,0 +1,21 @@
+package tasks.t10;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.GregorianCalendar;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class InDiffOfDateTest {
+    @Test
+    void test() {
+        GregorianCalendar dateTrue = new GregorianCalendar(2000, 1, 1);
+        boolean needTrue = InDiffOfDate.isBetween(dateTrue);
+
+        GregorianCalendar dateFalse = new GregorianCalendar(1999, 1, 1);
+        boolean needFalse = InDiffOfDate.isBetween(dateFalse);
+
+        assertTrue(needTrue);
+        assertFalse(needFalse);
+    }
+}
