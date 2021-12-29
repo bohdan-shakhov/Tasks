436f9ee8356076501acdea84e800fb0a0bf792f6
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-29 18:17:21 +0200

    updating for testing

--- src/main/java/tasks/t10/InDiffOfDate.java
+++ src/main/java/tasks/t10/InDiffOfDate.java
@@ -1,31 +1,17 @@
 package tasks.t10;
 
 import java.util.GregorianCalendar;
-import java.util.Scanner;
 
 public class InDiffOfDate {
     public static void main(String[] args) {
-
-        Scanner scanner = new Scanner(System.in);
-        System.out.println("Enter year: ");
-        int year = Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter month: ");
-        int month = Integer.parseInt(scanner.nextLine());
-        System.out.println("Enter day of month: ");
-        int day = Integer.parseInt(scanner.nextLine());
-
-        GregorianCalendar date = new GregorianCalendar(year, month, day);
+        GregorianCalendar date = new GregorianCalendar(2000, 1, 1);
         isBetween(date);
-
     }
 
-    static void isBetween(GregorianCalendar date) {
+    static boolean isBetween(GregorianCalendar date) {
         GregorianCalendar firstDate = new GregorianCalendar(2000, 1, 1);
         GregorianCalendar secondDate = new GregorianCalendar(2020, 12, 31);
-        if (date.getTimeInMillis() >= firstDate.getTimeInMillis()
-                && date.getTimeInMillis() <= secondDate.getTimeInMillis())
-            System.out.println("your date between 2000 and 2020");
-        else
-            System.out.println("your date NOT between 2000 and 2020");
+        return date.getTimeInMillis() >= firstDate.getTimeInMillis()
+                && date.getTimeInMillis() <= secondDate.getTimeInMillis();
     }
 }
