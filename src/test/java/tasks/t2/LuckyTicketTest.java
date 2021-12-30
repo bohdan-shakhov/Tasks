ac28c1c1f387af1f4ad25b5b0f70594b6c8c6a89
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-30 12:23:10 +0200

    add tests for LuckyTicket

---
+++ src/test/java/tasks/t2/LuckyTicketTest.java
@@ -0,0 +1,19 @@
+package tasks.t2;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class LuckyTicketTest {
+    @Test
+    void test() {
+        List<String> list = LuckyTicket.luckyTickets();
+        String first = list.get(0);
+        String last = list.get(list.size() - 1);
+
+        assertEquals("000000", first);
+        assertEquals("999999", last);
+    }
+}
