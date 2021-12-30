09e4c7567ba7d98cca81a0b8dc7c3dfac73da037
bohdan-shakhov <bogdanshahov2000@gmail.com>
2021-12-30 15:05:11 +0200

    add  correct tests for URIParsing

--- src/test/java/tasks/t7/URIParseTest.java
+++ src/test/java/tasks/t7/URIParseTest.java
@@ -11,8 +11,12 @@ public class URIParseTest {
         URIParse uriParse1 = URIParse.parse("http://www.ietf.org:80/rfc/rfc2396.txt?q=1&s1");
         assertEquals(uriParseTest1, uriParse1);
 
-        URIParse uriParseTest2 = new URIParse("", "", "", "", "");
+        URIParse uriParseTest2 = new URIParse("http", "www.ietf.org", null, "/rfc/rfc2396.txt", "q=1&s1");
         URIParse uriParse2 = URIParse.parse("http://www.ietf.org:/rfc/rfc2396.txt?q=1&s1");
+        assertEquals(uriParseTest2, uriParse2);
+
+        URIParse uriParseTest3 = new URIParse(null, "www.ietf.org", null, "/rfc/rfc2396.txt", "q=1&s1");
+        URIParse uriParse3 = URIParse.parse("://www.ietf.org:/rfc/rfc2396.txt?q=1&s1");
         assertEquals(uriParseTest2, uriParse2);
     }
 }
