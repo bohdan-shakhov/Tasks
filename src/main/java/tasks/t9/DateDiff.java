248daf5e36995be64830f286668c71c48bab8276
Bohdan Shakhov <bogdanshahov2000@gmail.com>
2022-01-03 15:47:19 +0200

    finished DateDiff task with normal output

--- src/main/java/tasks/t9/DateDiff.java
+++ src/main/java/tasks/t9/DateDiff.java
@@ -1,41 +1,48 @@
 package tasks.t9;
 
 import java.time.LocalDate;
+import java.time.Month;
+import java.time.Period;
 import java.time.temporal.ChronoUnit;
 
 public class DateDiff {
     public static void main(String[] args) {
-        LocalDate firstDate = LocalDate.of(2000, 5, 2);
-        LocalDate secondDate = LocalDate.of(2002, 7, 30);
+        LocalDate firstDate = LocalDate.of(2000, Month.MAY, 2);
+        LocalDate secondDate = LocalDate.of(2003, Month.JUNE, 3);
 
-        long years = ChronoUnit.YEARS.between(firstDate, secondDate);
-        long monthes = ChronoUnit.MONTHS.between(firstDate, secondDate);
-        long days = ChronoUnit.DAYS.between(firstDate, secondDate);
+        System.out.println(dateComparison(firstDate, secondDate));
+    }
+
+    private static StringBuilder dateComparison(LocalDate firstDate, LocalDate secondDate) {
+        StringBuilder result = new StringBuilder();
+        Period period = Period.between(firstDate, secondDate);
+        long years = Math.abs(period.getYears());
+        long months = Math.abs(period.getMonths());
+        long days = Math.abs(period.getDays());
 
         if ((years % 100 > 5 && years % 100 < 21) || years == 0) {
-            System.out.print(years + " лет ");
+            result.append(years + " лет ");
         } else if (years % 10 == 1) {
-            System.out.print(years + " год ");
+            result.append(years + " год ");
         } else if (years % 10 > 1 && years < 5) {
-            System.out.print(years + " года ");
+            result.append(years + " года ");
         }
 
-        long month = monthes - years * 12;
-        if ((month % 100 > 4 && month % 100 < 21) || month == 0) {
-            System.out.print(month + " месяцев ");
-        } else if (month % 10 == 1) {
-            System.out.print(month + " месяц ");
-        } else if (month % 10 > 1 && month < 5) {
-            System.out.print(month + " месяца ");
+        if ((months % 100 > 4 && months % 100 < 21) || months == 0) {
+            result.append(months + " месяцев ");
+        } else if (months % 10 == 1) {
+            result.append(months + " месяц ");
+        } else if (months % 10 > 1 && months < 5) {
+            result.append(months + " месяца ");
         }
 
-        long day = days - (days - month * 30);
-        if ((day % 100 > 1 && day % 100 < 5) || day == 0) {
-            System.out.print(day + " дня");
-        } else if (day % 10 == 1) {
-            System.out.print(day + " день");
+        if (days % 100 > 1 && days % 100 < 5) {
+            result.append(days + " дня ");
+        } else if (days % 10 == 1) {
+            result.append(days + " день ");
         } else {
-            System.out.print(day + " дней ");
+            result.append(days + " дней ");
         }
+        return result;
     }
 }
