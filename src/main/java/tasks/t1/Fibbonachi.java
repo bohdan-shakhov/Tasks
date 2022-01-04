4354caa1f6064b991db914b1e5aa28875a525ee4
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-04 16:51:09 +0200

    updating for tests

--- src/main/java/tasks/t1/Fibbonachi.java
+++ src/main/java/tasks/t1/Fibbonachi.java
@@ -2,16 +2,16 @@ package tasks.t1;
 
 public class Fibbonachi {
     public static void main(String[] args) {
-        fibbonachiSequence(10);
+        fibbonachiSequence(100);
     }
 
-    static int[] fibbonachiSequence(int n) {
-        int[] fnums = new int[n];
-        int num1 = 1;
-        int num2 = 1;
+    static long[] fibbonachiSequence(int n) {
+        long[] fnums = new long[n];
+        long num1 = 1L;
+        long num2 = 1L;
         fnums[0] = num1;
         fnums[1] = num2;
-        int num3;
+        long num3;
         System.out.print(num1 + " " + num2 + " ");
         for (int i = 3; i < n + 1; i++) {
             num3 = num1 + num2;
--- src/test/java/tasks/t1/FibbonachiTest.java
+++ src/test/java/tasks/t1/FibbonachiTest.java
@@ -6,11 +6,23 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FibbonachiTest {
     @Test
-    void test() {
-        int[] arr = Fibbonachi.fibbonachiSequence(10);
-
-        assertEquals(1, arr[0]);
-        assertEquals(55, arr[9]);
+    void testWithNormalN() {
+        long[] arr = new long[] {1, 1, 2, 3, 5, 8, 13, 21, 34, 55};
         assertArrayEquals(arr, Fibbonachi.fibbonachiSequence(10));
+    }
+
+    @Test
+    void testWithBigPositiveN() {
+
+    }
+
+    @Test
+    void testWitnNEqulsToZero() {
+
+    }
+
+    @Test
+    void testWithNegativeN() {
+
     }
 }
