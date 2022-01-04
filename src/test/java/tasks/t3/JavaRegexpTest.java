8c39ccfb99877d87ce61c740a8cac04264490918
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-04 17:26:09 +0200

    add test for negative case

--- src/test/java/tasks/t3/JavaRegexpTest.java
+++ src/test/java/tasks/t3/JavaRegexpTest.java
@@ -1,17 +1,31 @@
 package tasks.t3;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 class JavaRegexpTest {
-    @Test
-    void test() {
-        String text = "Versions: Java  5, Java 6, Java   7, Java 8, Java 12.";
-        ArrayList<String> list = JavaRegexp.javaSubstring(text);
+    List<String> list;
 
-        assertEquals("Java  5", list.get(0));
+    @Test
+    void testPositiveCase() {
+        String text = "Versions: Java  5, Java 6, Java   7, Java 8, Java 12.";
+        list = JavaRegexp.javaSubstring(text);
+
+        String expected = "Java5";
+        String actual = list.get(0);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testNegativeCase() {
+        String text = "asdfjsadfhkj gsadfjdfhkjlfhdlk;fjkl;sgjfd gjkjhdf akjnb fmnxczbv ,kljfhd lkjfh lkjfhd";
+        list = JavaRegexp.javaSubstring(text);
+
+        assertThrows(IndexOutOfBoundsException.class, () -> list.get(0));
     }
 }
