067ae90f1f85e0464febdfd3a608fbb73ec2dddf
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-04 18:06:02 +0200

    add tests for empty string and null

--- src/test/java/tasks/t4/WordTest.java
+++ src/test/java/tasks/t4/WordTest.java
@@ -8,7 +8,22 @@ class WordTest {
     @Test
     void test() {
         String text = "fffff ab hfhfhf abc fa 1234 jkjk";
-        String s = Word.specialWord(text);
-        assertEquals("fffff", s);
+        String expected = "fffff";
+        String actual = Word.specialWord(text);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testEmptyString() {
+        String text = "";
+        String expected = "";
+        String actual = Word.specialWord(text);
+        assertEquals(expected, actual);
+    }
+
+    @Test
+    void testForNull() {
+        String text = null;
+        assertThrows(NullPointerException.class, () -> Word.specialWord(text));
     }
 }
