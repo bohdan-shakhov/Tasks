67f263f98f8245ddeecc4c3dd490887c5af7e4aa
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-04 18:16:50 +0200

    add additional methods

--- src/main/java/tasks/t9/DateDiff.java
+++ src/main/java/tasks/t9/DateDiff.java
@@ -3,7 +3,6 @@ package tasks.t9;
 import java.time.LocalDate;
 import java.time.Month;
 import java.time.Period;
-import java.time.temporal.ChronoUnit;
 
 public class DateDiff {
     public static void main(String[] args) {
@@ -20,22 +19,14 @@ public class DateDiff {
         long months = Math.abs(period.getMonths());
         long days = Math.abs(period.getDays());
 
-        if ((years % 100 > 5 && years % 100 < 21) || years == 0) {
-            result.append(years).append(" лет ");
-        } else if (years % 10 == 1) {
-            result.append(years).append(" год ");
-        } else if (years % 10 > 1 && years < 5) {
-            result.append(years).append(" года ");
-        }
+        printYears(result, years);
+        printMonths(result, months);
+        printDays(result, days);
 
-        if ((months % 100 > 4 && months % 100 < 21) || months == 0) {
-            result.append(months).append(" месяцев ");
-        } else if (months % 10 == 1) {
-            result.append(months).append(" месяц ");
-        } else if (months % 10 > 1 && months < 5) {
-            result.append(months).append(" месяца ");
-        }
+        return result;
+    }
 
+    private static void printDays(StringBuilder result, long days) {
         if (days % 100 > 1 && days % 100 < 5) {
             result.append(days).append(" дня");
         } else if (days % 10 == 1) {
@@ -43,6 +34,25 @@ public class DateDiff {
         } else {
             result.append(days).append(" дней");
         }
-        return result;
+    }
+
+    private static void printMonths(StringBuilder result, long months) {
+        if ((months % 100 > 4 && months % 100 < 21) || months == 0) {
+            result.append(months).append(" месяцев ");
+        } else if (months % 10 == 1) {
+            result.append(months).append(" месяц ");
+        } else if (months % 10 > 1 && months < 5) {
+            result.append(months).append(" месяца ");
+        }
+    }
+
+    private static void printYears(StringBuilder result, long years) {
+        if ((years % 100 > 5 && years % 100 < 21) || years == 0) {
+            result.append(years).append(" лет ");
+        } else if (years % 10 == 1) {
+            result.append(years).append(" год ");
+        } else if (years % 10 > 1 && years < 5) {
+            result.append(years).append(" года ");
+        }
     }
 }
