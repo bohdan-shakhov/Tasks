0bdebd935cfe3629b702e4268d5c54e308cf1768
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-05 12:38:29 +0200

    increase speed of execution

--- src/main/java/tasks/t12/OptimizedCharsInString.java
+++ src/main/java/tasks/t12/OptimizedCharsInString.java
@@ -1,7 +1,6 @@
 package tasks.t12;
 
 import java.io.*;
-import java.util.Arrays;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -9,26 +8,28 @@ public class OptimizedCharsInString {
     public static void main(String[] args) {
         long before = System.currentTimeMillis();
         File file = new File("src/main/java/tasks/t12/1.txt");
-        Map<Character, Long> map = charactersCount(file);
-        System.out.println(map);
+        charactersCount(file);
         long after = System.currentTimeMillis();
-        System.out.println(after - before);
+
+        System.out.println((after - before) / Math.pow(10.0, 3.0) + " sec");
     }
 
-    static Map<Character, Long> charactersCount(File file) {
+    private static Map<Character, Long> charactersCount(File file) {
         Map<Character, Long> map = new HashMap<>();
-        try {
-            FileInputStream fis = new FileInputStream(file.getPath());
+        try (FileReader fileReader = new FileReader(file.getPath())) {
+            StreamTokenizer st = new StreamTokenizer(fileReader);
+            st.resetSyntax();
+            int token;
             char current;
-            while (fis.available() > 0) {
-                current = (char) fis.read();
-                    if (!Character.isLetter(current)) {
-                        continue;
-                    }
-                    if (!map.containsKey(current)) {
-                        map.put(current, 0L);
-                    }
-                    map.put(current, map.get(current) + 1);
+            while ((token = st.nextToken()) != StreamTokenizer.TT_EOF) {
+                current = (char) token;
+                if (!Character.isLetter(current)) {
+                    continue;
+                }
+                if (!map.containsKey(current)) {
+                    map.put(current, 0L);
+                }
+                map.put(current, map.get(current) + 1);
             }
         } catch (IOException e) {
             e.printStackTrace();
