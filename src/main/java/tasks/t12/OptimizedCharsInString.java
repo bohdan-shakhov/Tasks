de3e8ca2ee98a8134d52110cb72ee41f67fb94c5
bohdan-shakhov <bogdanshahov2000@gmail.com>
2022-01-05 17:10:43 +0200

    add method that fill the file

--- src/main/java/tasks/t12/OptimizedCharsInString.java
+++ src/main/java/tasks/t12/OptimizedCharsInString.java
@@ -8,10 +8,32 @@ public class OptimizedCharsInString {
     public static void main(String[] args) {
         long before = System.currentTimeMillis();
         File file = new File("src/main/java/tasks/t12/1.txt");
+        fillFile(file);
         charactersCount(file);
         long after = System.currentTimeMillis();
 
         System.out.println((after - before) / Math.pow(10.0, 3.0) + " sec");
+    }
+
+    private static void fillFile(File file) {
+        FileWriter fw;
+        long n = 5500000L;
+        try {
+            fw = new FileWriter(file);
+            BufferedWriter writer = new BufferedWriter(fw);
+            char character;
+            while (n > 0) {
+                int rnd = (int) (Math.random() * 52);
+                char base = (rnd < 26) ? 'A' : 'a';
+                character = (char) (base + rnd % 26);
+                writer.write(character);
+                n--;
+            }
+            writer.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
     }
 
     public static Map<Character, Long> charactersCount(File file) {
